e9ec7926a0c5f651be3491e2adb12d5e95311055
Pratham165 <140878099+Pratham165@users.noreply.github.com>
2023-08-21 09:13:53 +0530

    Add files via upload

---
+++ divide multiply by 2 without operator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("Enter the number a:");
+	scanf("%d",&a);
+	printf("Enter the number b:");
+	scanf("%d",&b);
+	a==a>>;
+	b==b<<;
+	printf("No. multiplied by 2="%d"","No. divided by 2="%d"",a,b);
+}

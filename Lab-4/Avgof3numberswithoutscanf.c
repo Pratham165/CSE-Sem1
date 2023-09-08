b4e249b711c693823f0e94f6528d553645fafd2a
Pratham165 <140878099+Pratham165@users.noreply.github.com>
2023-09-08 10:26:54 +0530

    Add files via upload

---
+++ Lab-4/Avgof3numberswithoutscanf.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b=10,c=10,d;
+	printf("Avg=%d",d=(a+b+c)/3);
+}
---
+++ Lab-4/hours.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int s,h,m;
+	printf("Input seconds:");
+	scanf("%d",&s);
+	h=s/3600;
+	m=(s%3600)/60;
+	s=s%60;
+	printf("Time is=%d:%d:%d",h,m,s);
+	return 0;
+	
+}
